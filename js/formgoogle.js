66188b4e3a0a7022c0f5dec34ab7af34dec537e5
wasupklyw <145723975+wasupklyw@users.noreply.github.com>
2024-06-21 16:38:42 +0300

    Add files via upload

---
+++ js/formgoogle.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+var iframe = document.getElementById('iframe1');
+iframe.className += ' formgoogle';
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var iframe = document.getElementById('iframe2');
+    iframe.className += ' formgoogle2';
+    });
